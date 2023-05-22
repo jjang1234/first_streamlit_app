ff350464602decec2d65b9ee3434d6debd5cad11
jjang1234 <134152371+jjang1234@users.noreply.github.com>
2023-05-22 23:26:25 +0900

    Update streamlit_app.py
    Fix spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.subheader('Breakfase Menu')
+streamlit.subheader('Breakfast Menu')
 streamlit.text('Omega 3  & Blueberry Oatmeal')
 streamlit.text('Kaye,Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
